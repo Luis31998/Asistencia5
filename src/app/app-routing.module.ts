e81da8c7e030c5487419b341d3065e3cc5a772ca
Luis31998 <xinoavaldivia@gmail.com>
2023-09-27 17:18:13 -0300

    routing partes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -15,6 +15,10 @@ const routes: Routes = [
     path: 'login',
     loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
   },
+  {
+    path: 'partes',
+    loadChildren: () => import('./partes/partes.module').then( m => m.PartesPageModule)
+  },
 ];
 
 @NgModule({
